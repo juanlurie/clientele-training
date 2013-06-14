69ff4700f3ada6196e34d75c09138a5c2e3b5210
Juan Lurie <juanl@synerics.com>
2013-06-14 14:42:25 +0200

    proxy and decorator project

---
+++ Module 3/05 Proxies and Decorators/AsbaBank.Core/AuthorizeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AsbaBank.Core
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AuthorizeAttribute : Attribute
+    {
+        public HashSet<string> Roles { get; private set; }
+
+        public AuthorizeAttribute(string role)
+        {
+            Roles = new HashSet<string>
+            {
+                role
+            };
+        }
+
+        public AuthorizeAttribute(params string[] roles)
+        {
+            Roles = new HashSet<string>(roles);
+        }
+
+        public override string ToString()
+        {
+            return String.Join(", ", Roles);
+        }
+    }
+}
---
+++ Module 3/05 Proxies and Decorators/AsbaBank.Infrastructure/CommandPublisherLoggerDecorator.cs
@@ -0,0 +1,20 @@
+﻿using AsbaBank.Core;
+using AsbaBank.Core.Commands;
+
+namespace AsbaBank.Infrastructure
+{
+    public class CommandPublisherLoggerDecorator : IPublishCommands
+    {
+        private readonly IPublishCommands commandPublisher;
+
+        public CommandPublisherLoggerDecorator(IPublishCommands commandPublisher)
+        {
+            this.commandPublisher = commandPublisher;
+        }
+
+        public void Publish(ICommand command)
+        {
+            commandPublisher.Publish(command);
+        }
+    }
+}
